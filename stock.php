98d4534d74114572b9e731669b0dd0d6c26f53c5
Exequiel Fernando <xqlfernando@gmail.com>
2025-04-29 13:52:23 +0800

    tite

--- stock.php
+++ stock.php
@@ -428,6 +428,7 @@
 									<div class="d-flex align-items-center">
 										<h4 class="card-title">Items</h4>
 										<div class="ms-auto">
+											<?php if($_SESSION['role_id'] != 3):?>
 											<button class="btn btn-primary btn-round me-3" data-bs-toggle="modal" data-bs-target="#addRowModal">
 												<i class="fa fa-plus"></i>
 												Add Item
@@ -436,6 +437,7 @@
 												<i class="fa fa-plus"></i>
 												Add Stock
 											</button>
+											<?php endif; ?>
 										</div>
 										
 									</div>
