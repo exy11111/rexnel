98dd6612f8d620348082cd810531e73fb3bff3cc
Exequiel Fernando <xqlfernando@gmail.com>
2025-05-06 03:30:58 +0800

    a

--- index.php
+++ index.php
@@ -118,7 +118,7 @@
 						</li>
 						<?php endif; ?>
 						<li class="nav-item text-white">
-							<a href="index.php" class="text-white">
+							<a href="index.php" class="text-primary">
 								<i class="fas fa-home"></i>
 								<p>Dashboard</p>
 							</a>
