f3695c9442a3b40e2384fc1b891e2f612d78251d
Exequiel Fernando <xqlfernando@gmail.com>
2025-05-06 03:52:07 +0800

    a

--- login.php
+++ login.php
@@ -59,7 +59,7 @@
 <body>
 
   <div class="container d-flex justify-content-center align-items-center vh-100">
-    <div class="card p-3" style="width: 100%; max-width: 400px; max-height: 400px">
+    <div class="card p-3" style="width: 100%; max-width: 400px;">
       <div class="card-body">
       <div class="text-center mb-4">
         <img src="assets/img/lg.png" alt="Login Image" class="img-fluid" style="max-width: 150px;">
