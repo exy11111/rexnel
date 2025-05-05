8473f72d8e02cb54a10f9b0b492c6f5f28430534
Exequiel Fernando <xqlfernando@gmail.com>
2025-05-06 03:23:03 +0800

    a

--- index.php
+++ index.php
@@ -73,7 +73,10 @@
 			transform: scale(1.05);
 			box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
 			background-color: #f8f9fa;
-		}
+		}.nav-item.active a {
+	background-color: #0d6efd !important; /* or any highlight color */
+	color: #fff !important;
+}
 
 	</style>
 
