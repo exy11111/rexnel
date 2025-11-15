5ae18bb155ba7fb79a72fcef91d0d3c74f20b423
Exequiel Fernando <xqlfernando@gmail.com>
2025-11-15 11:10:22 +0800

    swap buttons

--- orderssupplier.php
+++ orderssupplier.php
@@ -151,14 +151,15 @@
 														echo "<td><span class='badge $badgeClass'>$status</span></td>";
 														echo "<td>
                                                                 <div class='form-button-action'>";
-														if($row['status'] != "Received"){
-															echo "<button type='button' class='btn btn-link btn-primary btn-lg' data-bs-toggle='modal' data-bs-target='#editStatusModal' data-id='".htmlspecialchars($row['order_id'])."' title='Edit Task'>
-                                                                        <i class='fa fa-edit'></i>
-                                                                    </button>";
-														}
                                                         	echo "<a href='#' class='btn btn-link btn-primary btn-lg' data-bs-toggle='modal' data-bs-target='#viewItemModal' data-id='".htmlspecialchars($row['order_id'])."' title='Edit Task'>
                                                                         <i class='bi bi-eye-fill'></i>
-                                                                    </a>
+                                                                    </a>";
+																	if($row['status'] != "Received"){
+																		echo "<button type='button' class='btn btn-link btn-primary btn-lg' data-bs-toggle='modal' data-bs-target='#editStatusModal' data-id='".htmlspecialchars($row['order_id'])."' title='Edit Task'>
+																					<i class='fa fa-edit'></i>
+																				</button>";
+																	}
+																	echo "
                                                                 </div>
                                                             </td>";
                                                         echo "</tr>";
