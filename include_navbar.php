c73a46920d811c7f6eee48926fbf242d8f1f8a62
Exequiel Fernando <xqlfernando@gmail.com>
2025-11-15 11:16:30 +0800

    navbar color

--- include_navbar.php
+++ include_navbar.php
@@ -1,4 +1,4 @@
-<div class="main-header">
+<div class="main-header" style="background-color: #000 !important;">
     <div class="main-header-logo">
         <!-- Logo Header -->
         <div class="logo-header" data-background-color="dark">
