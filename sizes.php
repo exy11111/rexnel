f03240cba5d15f0fc5ee75620e4cb442b03db5a4
Exequiel Fernando <xqlfernando@gmail.com>
2025-11-20 14:28:53 +0800

    sizes centralized

--- sizes.php
+++ sizes.php
@@ -3,9 +3,8 @@
 	require ('db.php');
 	$branch_id = $_SESSION['branch_id'];
 
-	$sql = "SELECT * FROM sizes WHERE branch_id = :branch_id";
+	$sql = "SELECT * FROM sizes";
     $stmt = $conn->prepare($sql);
-	$stmt->bindParam(':branch_id', $branch_id);
     $stmt->execute();
     $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -50,14 +49,6 @@
 			<div class="container">
 				<div class="page-inner">
 					<div class="page-header">
-						<?php 
-						$sql = "SELECT branch_name FROM branch WHERE branch_id = :branch_id";
-						$stmt = $conn->prepare($sql);
-						$stmt->bindParam(':branch_id', $_SESSION['branch_id']);
-						$stmt->execute();
-						$branch_name = $stmt->fetchColumn();
-						?>
-						<h3 class="fw-bold mb-3"><?php echo $branch_name; ?></h3>
 						<ul class="breadcrumbs mb-3">
 							<li class="nav-home">
 								<a href="index.php">
