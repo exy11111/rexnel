12614bcffd4dfb693ebab5806cf7338edd6b01ce
Exequiel Fernando <xqlfernando@gmail.com>
2025-11-20 14:10:37 +0800

    debug 9

--- stock.php
+++ stock.php
@@ -92,7 +92,16 @@
 						<?php 
 						$sql = "SELECT branch_name FROM branch WHERE branch_id = :branch_id";
 						$stmt = $conn->prepare($sql);
-						$stmt->bindParam(':branch_id', $_SESSION['branch_id']);
+						if(isset($_GET['b'])){
+							$stmt->bindParam(':branch_id', $_GET['b']);
+						}
+						else if($_SESSION['branch_id'] == 0){
+							$branch_id = 1;
+							$stmt->bindParam(':branch_id', $branch_id);
+						}
+						else{
+							$stmt->bindParam(':branch_id', $_SESSION['branch_id']);
+						}
 						$stmt->execute();
 						$branch_name = $stmt->fetchColumn();
 						?>
