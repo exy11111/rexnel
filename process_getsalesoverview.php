39ad161ac3f64524462c44f5cdf2fdd9197cafea
Exequiel Fernando <xqlfernando@gmail.com>
2025-11-20 15:13:30 +0800

    debug 4

--- process_getsalesoverview.php
+++ process_getsalesoverview.php
@@ -1,6 +1,9 @@
 <?php
     require ('db.php');
     require ('session.php');
+    ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
     $start = $_GET['start'] ?? null;
     $end = $_GET['end'] ?? null;
