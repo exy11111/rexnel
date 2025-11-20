c384c962cfbf44240ff84aaa36e773d395485f6b
Exequiel Fernando <xqlfernando@gmail.com>
2025-11-20 14:41:35 +0800

    debug

--- staff.php
+++ staff.php
@@ -132,13 +132,8 @@
 															<div class="col-sm-12 <?php if($_SESSION['role_id'] != 1): echo 'd-none'; endif; ?>">
                                                                 <div class="form-group form-group-default">
                                                                     <label class="<?php if($_SESSION['role_id'] != 1): echo 'd-none'; endif; ?>">Role</label>
-                                                                    <select name="role_id" class="form-select <?php if($_SESSION['role_id'] != 1): echo 'd-none'; endif; ?>" value="<?php if($_SESSION['role_id'] != 1): echo '3'; endif; ?>">
-																		<option value="">Select Role</option>
-																		<?php foreach($roles_data as $row):?>
-                                                                            <?php if($row['role_id'] != 3):?>
-																			    <option value="<?php echo $row['role_id']?>"><?php echo $row['role_name']?></option>
-                                                                            <?php endif; ?>
-																		<?php endforeach; ?>
+                                                                    <select name="role_id" class="form-select <?php if($_SESSION['role_id'] != 1): echo 'd-none'; endif; ?>">
+                                                                        <option value="2">Branch Admin</option>
 																	</select>
                                                                 </div>
                                                             </div>
