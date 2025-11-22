a2b35bf111177e2f14a034acc3ca711c499336d1
Exequiel Fernando <xqlfernando@gmail.com>
2025-11-22 18:09:36 +0800

    9

--- process_addexpense.php
+++ process_addexpense.php
@@ -7,15 +7,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $amount = $_POST['amount'];
     $comment = $_POST['comment'];
     $branch_id = $_SESSION['branch_id'];
+    $timestamp = date('Y-m-d H:i:s');
 
     try {
         
-        $sql = "INSERT INTO expenses (expensetype_id, amount, comment, branch_id) VALUES (:expensetype_id, :amount, :comment, :branch_id)";
+        $sql = "INSERT INTO expenses (expensetype_id, amount, comment, branch_id, created_at) VALUES (:expensetype_id, :amount, :comment, :branch_id, :created_at)";
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':expensetype_id', $expensetype_id);
         $stmt->bindParam(':amount', $amount);
         $stmt->bindParam(':comment', $comment);
         $stmt->bindParam(':branch_id', $branch_id);
+        $stmt->bindParam(':created_at', $timestamp);
         $stmt->execute();
 
         header("Location: expenses.php?status=success");
