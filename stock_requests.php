0066f028c6275f4f0b535ccd103781081cb026c1
Exequiel Fernando <xqlfernando@gmail.com>
2025-11-24 04:28:04 +0800

    2

--- stock_requests.php
+++ stock_requests.php
@@ -1,6 +1,9 @@
 <?php 
 	require ('session.php');
 	require ('db.php');
+    ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 	if(isset($_GET['b'])){
 		$_SESSION['branch_id'] = $_GET['b'];
