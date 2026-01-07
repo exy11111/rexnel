413cde778117c1aa020d8960dcc70d975e5203ae
Exequiel Fernando <xqlfernando@gmail.com>
2026-01-08 02:31:15 +0800

    16

--- process_getreceiptdata.php
+++ process_getreceiptdata.php
@@ -9,38 +9,27 @@ if (isset($_GET['item_id'])) {
     $sql = "
     SELECT 
         i.item_name,
-        pi.quantity,
+        i.stock,
         s.size_name,
 
-        /* Price per unit (discount-aware) */
+        /* Current selling price */
         CASE
-            WHEN pi.is_discounted = 1 
-                AND pi.unit_price IS NOT NULL
-            THEN pi.unit_price
+            WHEN i.is_discounted = 1 
+                 AND i.discount_price IS NOT NULL
+            THEN i.discount_price
             ELSE i.price
         END AS unit_price,
 
-        /* Total per line */
-        pi.quantity * 
-        (
-            CASE
-                WHEN pi.is_discounted = 1 
-                    AND pi.unit_price IS NOT NULL
-                THEN pi.unit_price
-                ELSE i.price
-            END
-        ) AS item_price,
+        i.is_discounted
 
-        pi.is_discounted
-
-    FROM purchase_items pi
-    JOIN items i ON i.item_id = pi.item_id
+    FROM items i
     JOIN sizes s ON i.size_id = s.size_id
-    WHERE pi.purchase_id = :purchase_id
+    WHERE i.item_id = :item_id
+      AND i.is_disabled = 0
     ";
 
     $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':purchase_id', $purchase_id);
+    $stmt->bindParam(':item_id', $item_id);
     $stmt->execute();
 
     if ($stmt->rowCount() > 0) {
